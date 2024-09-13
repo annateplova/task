15bc1b74876250cc302c0a3b89b1b98984422b96
annateplova <166602583+annateplova@users.noreply.github.com>
2024-09-13 10:11:09 +0200

    Add files via upload

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,17 +1,29 @@
-import {Component, EventEmitter, Output} from "@angular/core";
+import {Component, EventEmitter, Output, signal} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {type NewTaskData} from "../task/task.model";
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './new-task.component.html',
   styleUrls: ['./new-task.component.css']
 })
 
 export class NewTaskComponent {
   @Output() cancel = new EventEmitter<void>();
+  @Output() add = new EventEmitter<NewTaskData>();
+  enteredTitle = signal('');
+  enteredSummary = signal('');
+  enteredDate = signal('');
 
   onCancel() {
     this.cancel.emit();
+  }
+
+  onSubmit() {
+
   }
 }
